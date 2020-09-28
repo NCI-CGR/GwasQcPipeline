ec28b88df7a61f10a30ed6c207b5d73a3a7ef37e
Justin Fear <justin.fear@nih.gov>
2020-09-28 10:26:14 -0400

    Moves file names to a conftest.
    I am going to be using BPM/GTC/sample_sheet across multiple tests. Moved these file name fixutres out into a conftest so I can just refer to them.

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def gtc_file():
+    return Path("tests/data/illumina/gtc/small_genotype.gtc")
+
+
+@pytest.fixture(scope="session")
+def bpm_file():
+    return Path("tests/data/illumina/bpm/small_manifest.bpm")
+
+
+@pytest.fixture(scope="session")
+def sample_sheet_file():
+    return Path("tests/data/example_sample_sheet.csv")
--- tests/unit/parsers/test_illumina_gtc.py
+++ tests/unit/parsers/test_illumina_gtc.py
@@ -4,8 +4,8 @@ from cgr_gwas_qc.parsers.illumina import GenotypeCalls
 
 
 @pytest.fixture(scope="module")
-def gtc() -> GenotypeCalls:
-    return GenotypeCalls("tests/data/illumina/gtc/small_genotype.gtc")
+def gtc(gtc_file) -> GenotypeCalls:
+    return GenotypeCalls(gtc_file)
 
 
 def test_length_of_gtc_toc(gtc):
