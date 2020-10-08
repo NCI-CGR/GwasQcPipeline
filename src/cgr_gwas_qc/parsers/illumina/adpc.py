e6ad345c4bb6f09cb86e6db6fafe90dc3aaefb7d
Justin Fear <justin.fear@nih.gov>
2020-10-08 08:43:54 -0400

    Cleaned up docstring and classes.

--- src/cgr_gwas_qc/parsers/illumina/adpc.py
+++ src/cgr_gwas_qc/parsers/illumina/adpc.py
@@ -15,7 +15,7 @@ class AdpcRecord:
         genotype_score: float,
         genotype: int,
     ):
-        """A record from an Adpc file.
+        """A record from Illumina's adpc.bin file.
 
         Args:
             x_raw: A allele intensity
@@ -25,7 +25,7 @@ class AdpcRecord:
             genotype_score: Genotype call score (a.k.a. clustering confidence)
             genotype: Genotype call (0: AA, 1: AB, 2: BB, 3: unknown/missing)
         """
-        # Coerce to specific types b/c I am dealing with reading/writing binary data.
+        # Coerce to specific C-types b/c I am dealing with reading/writing binary data.
         self.x_raw = uint16(x_raw)
         self.y_raw = uint16(y_raw)
         self.x_norm = float32(x_norm)
@@ -68,9 +68,11 @@ class AdpcRecord:
 
 
 class AdpcBase:
-    """Base class for Adpc files.
+    """Base class for Illumina's adpc.bin files.
 
-    See https://javadoc.io/static/org.broadinstitute/gatk/4.1.4.1/picard/arrays/illumina/IlluminaAdpcFileWriter.html
+    Based on information found on Picard's website:
+
+    https://javadoc.io/static/org.broadinstitute/gatk/4.1.4.1/picard/arrays/illumina/IlluminaAdpcFileWriter.html
     """
 
     padding_struct = struct.Struct("<8H")  # There is a 16 byte padding
@@ -79,35 +81,40 @@ class AdpcBase:
 
 class AdpcReader(AdpcBase):
     def __init__(self, file_name: Union[str, Path]):
-        """Adpc reader context manager.
+        """A context manager for reading adpc.bin formatted files.
 
         Args:
-            file_name: Path to an adpc file.
+            file_name: Path to an adpc.bin file.
 
         Example:
-            >>> file_name = "../../../tests/data/small.adpc.bin"
             >>> with AdpcReader(file_name) as fh:
             ...     for record in fh:
             ...         print(record)
         """
         self.file_name = Path(file_name)
 
-    def __enter__(self):
+    def open(self):
         self.file_handler = self.file_name.open("rb")
-        self.file_handler.read(16)  # skip padding
-        return self
-
-    def __exit__(self, *args):
-        self.close()
 
     def close(self):
         self.file_handler.close()
+
+    def read(self, bytes):
+        return self.file_handler.read(bytes)
+
+    def __enter__(self):
+        self.open()
+        self.read(16)  # skip padding
+        return self
+
+    def __exit__(self, *exc):
+        self.close()
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        data_bytes = self.file_handler.read(18)
+        data_bytes = self.read(18)
         if data_bytes:
             return AdpcRecord(*self.record_struct.unpack(data_bytes))
         else:
@@ -116,28 +123,38 @@ class AdpcReader(AdpcBase):
 
 class AdpcWriter(AdpcBase):
     def __init__(self, file_name: Union[str, Path]):
-        """Adpc writer context manager.
+        """A context manager for writing adpc.bin formatted files.
 
         Args:
             file_name: Path to save an adpc file.
 
         Example:
-            >>> file_name = "/tmp/small.adpc.bin"
             >>> with AdpcReader(file_name) as fh:
             ...    fh.write(record)
         """
         self.file_name = Path(file_name)
 
-    def __enter__(self):
+    def open(self):
         self.file_handler = self.file_name.open("wb")
-        self.file_handler.write(self.padding_struct.pack(*[0] * 8))  # add padding
-        return self
-
-    def __exit__(self, *args):
-        self.file_handler.close()
 
     def close(self):
         self.file_handler.close()
 
     def write(self, record: AdpcRecord):
         self.file_handler.write(self.record_struct.pack(*record))
+
+    def _add_padding(self):
+        """The adpc.bin format has 16 bytes of padding at the begining of the file.
+
+        The Picard group did not know what this is for, but they said it is
+        not needed so I will just add 8 0s at 2 bytes each.
+        """
+        self.file_handler.write(self.padding_struct.pack(*[0] * 8))
+
+    def __enter__(self):
+        self.open()
+        self._add_padding()
+        return self
+
+    def __exit__(self, *exc):
+        self.close()
