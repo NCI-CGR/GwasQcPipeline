9a82f93e2fde416bc91d9ee3300f17c95a056277
Justin Fear <justin.fear@nih.gov>
2020-10-09 18:09:10 -0400

    Adds fixture to be a test working dir.
    Adds a fixture `working_dir` that is meant to simulate a users working directory with a config and sample sheet and some files.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import pytest
@@ -33,3 +34,23 @@ def vcf(vcf_file):
     import pysam
 
     return pysam.VariantFile(vcf_file, "r")
+
+
+@pytest.fixture(scope="module")
+def working_dir(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("user_currdir")
+
+    # copy test data to working dir
+    shutil.copy2("tests/data/config.yml", tmp_path)
+    shutil.copy2("tests/data/example_sample_sheet.csv", tmp_path)
+    shutil.copy2("tests/data/illumina/bpm/small_manifest.bpm", tmp_path)
+    shutil.copy2("tests/data/1KG/small_1KG.vcf.gz", tmp_path)
+    shutil.copy2("tests/data/1KG/small_1KG.vcf.gz.tbi", tmp_path)
+    shutil.copyfile(
+        "tests/data/illumina/gtc/small_genotype.gtc", tmp_path / "SB00001_PB0001_A01.gtc"
+    )
+    shutil.copyfile(
+        "tests/data/illumina/gtc/small_genotype.gtc", tmp_path / "SB00002_PB0001_B01.gtc"
+    )
+
+    return tmp_path
