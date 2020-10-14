3e1233cf7e481584f10cfdd25c415a35be130e18
Justin Fear <justin.fear@nih.gov>
2020-10-14 16:03:32 -0400

    Partially functional pre-flight.

--- src/cgr_gwas_qc/cli/pre_flight.py
+++ src/cgr_gwas_qc/cli/pre_flight.py
@@ -1,10 +1,14 @@
+from collections import defaultdict
 from logging import getLogger
 from pathlib import Path
+from textwrap import indent
+from typing import Dict, List
 
 import typer
 from snakemake.rules import expand
 
 from cgr_gwas_qc import load_config
+from cgr_gwas_qc.validators import GwasQcValidationError
 from cgr_gwas_qc.validators.gtc import validate as gtc_validate
 
 app = typer.Typer(add_completion=False)
@@ -24,11 +28,63 @@ def main(gtc_check: bool = True, idat_check: bool = False):
 
     if idat_check:
         logger.warn("Idat pre-flight is not impelmented yet.")
+        # check_idat_files(expand(cfg.config.user_data_patterns.idat.red, **cfg.ss.to_dict("list")), "Red")
+        # check_idat_files(expand(cfg.config.user_data_patterns.idat.grn, **cfg.ss.to_dict("list")), "Green")
 
     if gtc_check:
-        for gtc in expand(cfg.config.user_data_patterns.gtc, **cfg.ss.to_dict("list")):
+        check_gtc_files(expand(cfg.config.user_data_patterns.gtc, **cfg.ss.to_dict("list")))
+
+
+def check_idat_files(files: List[str], color: str):
+    problems = defaultdict(list)
+    for idat in files:
+        try:
+            raise NotImplementedError
+        except FileNotFoundError:
+            problems["FileNotFound"].append(idat)
+        except PermissionError:
+            problems["FileNotReadable"].append(idat)
+        except GwasQcValidationError as err:
+            problems[err.args[0]].append(idat)
+
+    if problems:
+        typer.echo(
+            "There was a problem with these Idat {} files:\n{}".format(
+                color, pretty_print_paths(problems)
+            )
+        )
+    else:
+        typer.echo(f"Idat {color} Files OK.")
+
+
+def check_gtc_files(files: List[str]):
+    problems = defaultdict(list)
+    for gtc in files:
+        try:
             gtc_validate(Path(gtc))
+        except FileNotFoundError:
+            problems["FileNotFound"].append(gtc)
+        except PermissionError:
+            problems["FileNotReadable"].append(gtc)
+        except GwasQcValidationError as err:
+            problems[err.args[0]].append(gtc)
+
+    if problems:
+        typer.echo(
+            "There was a problem with these GTC files:\n{}".format(pretty_print_paths(problems))
+        )
+    else:
         typer.echo("GTC Files OK.")
+
+
+def pretty_print_paths(data: Dict[str, List[str]]) -> str:
+    """For each exception output a list of files nicely."""
+    output = ""
+    for k, v in data.items():
+        output += f"  {k}:\n"
+        files = "\n".join(sorted(v))
+        output += f"{indent(files, '    - ')}\n"
+    return output
 
 
 if __name__ == "__main__":
