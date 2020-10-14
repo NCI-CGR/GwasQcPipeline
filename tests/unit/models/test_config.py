6d33db07a21718c79a6c27f1d2d4f4ffa13c8584
Justin Fear <justin.fear@nih.gov>
2020-10-14 15:16:51 -0400

    Little config unit test to see how things work.

---
+++ tests/unit/models/test_config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from cgr_gwas_qc.models.config import Config
+from cgr_gwas_qc.testing import chdir
+from cgr_gwas_qc.yaml import load
+
+
+def test_config_model(working_dir):
+    with chdir(working_dir):
+        cfg = Config(**load(Path("config.yml")))
+        assert cfg.project_name == "Test Project"
+        assert cfg.sample_sheet == Path("example_sample_sheet.csv")
