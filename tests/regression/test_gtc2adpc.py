b79568047b4346fde049d2e6aa2965674f9faf1d
Justin Fear <justin.fear@nih.gov>
2020-10-14 14:11:00 -0400

    Adds comments and simplifies.

--- tests/regression/test_gtc2adpc.py
+++ tests/regression/test_gtc2adpc.py
@@ -10,33 +10,38 @@ from cgr_gwas_qc.workflow.scripts.gtc2adpc import app
 runner = CliRunner()
 
 
-@pytest.fixture
-def expected_adpc():
-    return Path("tests/data/small.adpc.bin")
-
-
-@pytest.fixture
-def expected_snp_count():
-    return Path("tests/data/small.adpc.bin.numSnps.txt")
-
-
+################################################################################
+# Compare to outputs from old script.
+################################################################################
 @pytest.mark.regression
-def test_gtc2adpc(gtc_file, bpm_file, expected_adpc, expected_snp_count, tmpdir):
+def test_gtc2adpc(gtc_file, bpm_file, tmpdir):
+    expected_adpc = Path("tests/data/small.adpc.bin")
+    expected_snp_count = Path("tests/data/small.adpc.bin.numSnps.txt")
+
     obs_adpc = Path(tmpdir) / "adpc.bin"
     obs_snp_count = Path(tmpdir) / "snp_counts.txt"
 
+    # Run the script
     results = runner.invoke(
         app,
         [gtc_file.as_posix(), bpm_file.as_posix(), obs_adpc.as_posix(), obs_snp_count.as_posix()],
     )
     assert results.exit_code == 0
+
+    # Make sure snp counts are identical
     assert expected_snp_count.read_text() == obs_snp_count.read_text()
 
+    # Make sure outputs are about equal. The output contains floats, due to
+    # rounding differences between py2 and py3 it is impossible to get the
+    # exact same number. However, they should be extremely close.
     with AdpcReader(expected_adpc) as expect, AdpcReader(obs_adpc) as obs:
         for exp_row, obs_row in zip(expect, obs):
+            # ints should be the exact same
             assert exp_row.x_raw == obs_row.x_raw
             assert exp_row.y_raw == obs_row.y_raw
+            assert exp_row.genotype == obs_row.genotype
+
+            # floats should be really close
             assert isclose(exp_row.x_norm, obs_row.x_norm)
             assert isclose(exp_row.y_norm, obs_row.y_norm)
             assert isclose(exp_row.genotype_score, obs_row.genotype_score)
-            assert exp_row.genotype == obs_row.genotype
