86048839a118e8a5d65deb6d65060ab7ee5751f3
Justin Fear <justin.fear@nih.gov>
2020-11-05 10:29:20 -0500

    Adds testing for current entry points.

--- tests/workflow/test_entry_points.py
+++ tests/workflow/test_entry_points.py
@@ -1,35 +1,51 @@
 from pathlib import Path
 from shutil import copytree
 from subprocess import run
-from textwrap import dedent
 
 import pytest
 
 from cgr_gwas_qc.testing import chdir
 
+snakefile = """\
+from cgr_gwas_qc import load_config
 
-@pytest.mark.workflow
-def test_gtc_to_plink(tmp_path: Path, gtc_working_dir: Path):
-    copytree(gtc_working_dir, tmp_path, dirs_exist_ok=True)
-    snake = tmp_path / "Snakefile"
-    snake.write_text(
-        dedent(
-            """\
-    from cgr_gwas_qc import load_config
+cfg = load_config()
 
-    cfg = load_config()
+include: cfg.rules("entry_points.smk")
 
-    include: cfg.rules("entry_points.smk")
+rule all:
+    input:
+        expand("plink_start/samples.{ext}", ext=["bed", "bim", "fam"])
+"""
 
-    rule all:
-        input:
-            expand("plink_start/samples.{ext}", ext=["bed", "bim", "fam"])
-    """
-        )
-    )
 
+def run_and_check_for_plink_start_samples(tmp_path):
     with chdir(tmp_path):
         run(["snakemake", "-j1", "--use-conda", "--nocolor"], check=True)
         assert Path("plink_start/samples.bed").exists()
         assert Path("plink_start/samples.bim").exists()
         assert Path("plink_start/samples.fam").exists()
+
+
+@pytest.mark.workflow
+def test_gtc_entry(tmp_path: Path, gtc_working_dir: Path):
+    copytree(gtc_working_dir, tmp_path, dirs_exist_ok=True)
+    snake = tmp_path / "Snakefile"
+    snake.write_text(snakefile)
+    run_and_check_for_plink_start_samples(tmp_path)
+
+
+@pytest.mark.workflow
+def test_ped_entry(tmp_path: Path, ped_working_dir: Path):
+    copytree(ped_working_dir, tmp_path, dirs_exist_ok=True)
+    snake = tmp_path / "Snakefile"
+    snake.write_text(snakefile)
+    run_and_check_for_plink_start_samples(tmp_path)
+
+
+@pytest.mark.workflow
+def test_bed_entry(tmp_path: Path, bed_working_dir: Path):
+    copytree(bed_working_dir, tmp_path, dirs_exist_ok=True)
+    snake = tmp_path / "Snakefile"
+    snake.write_text(snakefile)
+    run_and_check_for_plink_start_samples(tmp_path)
