121c2e126cc9129f74a695fe67f9c18c84c2ada0
Justin Fear <justin.fear@nih.gov>
2020-11-05 17:05:13 -0500

    Removes defaults.

--- src/cgr_gwas_qc/models/config/reference_files.py
+++ src/cgr_gwas_qc/models/config/reference_files.py
@@ -4,21 +4,14 @@ from pydantic import BaseModel, Field, FilePath, validator
 class ReferenceFiles(BaseModel):
     """A list of reference files used by the pipeline."""
 
-    illumina_manifest_file: FilePath = Field(
-        "/DCEG/CGF/Infinium/Resources/Manifests/GSAMD-Files/build37/GSAMD-24v1-0_20011747_A1.bpm",
-        description="Path to the array BPM file.",
-    )
+    illumina_manifest_file: FilePath = Field(..., description="Path to the array BPM file.")
 
     # illumina_cluster_file: FilePath = Field("", description="Path to the array cluster EGT file.")
 
-    thousand_genome_vcf: FilePath = Field(
-        "/DCEG/CGF/Bioinformatics/Production/data/thousG/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.vcf.gz",
-        description="Path to the 1KG VCF file.",
-    )
+    thousand_genome_vcf: FilePath = Field(..., description="Path to the 1KG VCF file.")
 
     thousand_genome_tbi: FilePath = Field(
-        "/DCEG/CGF/Bioinformatics/Production/data/thousG/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.vcf.gz.tbi",
-        description="Path to the corresponding index for the 1KG VCF file.",
+        ..., description="Path to the corresponding index for the 1KG VCF file."
     )
 
     @validator("illumina_manifest_file")
