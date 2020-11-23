903ab3bff644c7e744f394be82033a5c0f13f5c9
Justin Fear <justin.fear@nih.gov>
2020-11-23 16:54:30 -0500

    refactor(tests): refactor config model test to be more focused
    Refactors to focus on testing the different config models instead of the config manager's ability to load a config.

--- tests/models/test_config_data_model.py
+++ tests/models/test_config_data_model.py
@@ -3,73 +3,102 @@ from pathlib import Path
 import pytest
 from pydantic import ValidationError
 
-from cgr_gwas_qc import load_config
-from cgr_gwas_qc.models import config as cfg_models
+from cgr_gwas_qc.models.config import (
+    Config,
+    ReferenceFiles,
+    SoftwareParams,
+    UserFiles,
+    WorkflowParams,
+)
 from cgr_gwas_qc.testing import chdir
 from cgr_gwas_qc.version import __version__
 
 
-def test_load_config_from_yml(gtc_working_dir):
-    """Make sure loading YAML config gives expected results.
-
-    This is just a sanity check. Assuming pydantic works as expected, then
-    this should always pass unless we change default values.
-    """
-    with chdir(gtc_working_dir):
-        cfg = load_config()
-        assert cfg.config.pipeline_version == __version__
-        assert cfg.config.project_name == "Test Project"
-        assert cfg.config.sample_sheet == Path("example_sample_sheet.csv")
-
-        # Reference Files
-        assert cfg.config.reference_files.illumina_manifest_file == Path("small_manifest.bpm")
-        assert cfg.config.reference_files.thousand_genome_vcf == Path("small_1KG.vcf.gz")
-        assert cfg.config.reference_files.thousand_genome_tbi == Path("small_1KG.vcf.gz.tbi")
-
-        # User Files
-        # Since we are using the ``gtc_working_dir`` only GTC and IDAT should be defined.
-        assert cfg.config.user_files.gtc_pattern == "{Sample_ID}.gtc"
-        assert (
-            cfg.config.user_files.idat_pattern.red
-            == "{SentrixBarcode_A}_{SentrixPosition_A}_Red.idat"
-        )
-        assert (
-            cfg.config.user_files.idat_pattern.green
-            == "{SentrixBarcode_A}_{SentrixPosition_A}_Grn.idat"
+def test_reference_files_must_exist():
+    with pytest.raises(ValidationError):
+        ReferenceFiles(
+            illumina_manifest_file="test.bpm",
+            thousand_genome_vcf="test.vcf.gz",
+            thousand_genome_tbi="test.vcf.gz.tbi",
         )
 
-        assert cfg.config.user_files.ped is None
-        assert cfg.config.user_files.map is None
 
-        assert cfg.config.user_files.bed is None
-        assert cfg.config.user_files.bim is None
-        assert cfg.config.user_files.fam is None
+def test_reference_files(tmp_path):
+    with chdir(tmp_path):
+        [Path(x).touch() for x in ("test.bpm", "test.vcf.gz", "test.vcf.gz.tbi")]
 
-        # Software Params
-        assert cfg.config.software_params.samp_cr_1 == 0.8
-        assert cfg.config.software_params.snp_cr_1 == 0.8
-        assert cfg.config.software_params.samp_cr_2 == 0.95
-        assert cfg.config.software_params.snp_cr_2 == 0.95
-        assert cfg.config.software_params.ld_prune_r2 == 0.1
-        assert cfg.config.software_params.maf_for_ibd == 0.2
-        assert cfg.config.software_params.ibd_pi_hat_cutoff == 0.95
-        assert cfg.config.software_params.dup_concordance_cutoff == 0.95
-        assert cfg.config.software_params.contam_threshold == 0.2
-        assert cfg.config.software_params.contam_population == "AF"
-        assert cfg.config.software_params.pi_hat_threshold == 0.2
-        assert cfg.config.software_params.autosomal_het_threshold == 0.1
-        assert cfg.config.software_params.minimum_pop_subjects == 50
-        assert cfg.config.software_params.control_hwp_threshold == 50
-        assert (
-            cfg.config.software_params.strand == "top"
-        )  # Note we convert strand to lowercase on import
+        refs = ReferenceFiles(
+            illumina_manifest_file="test.bpm",
+            thousand_genome_vcf="test.vcf.gz",
+            thousand_genome_tbi="test.vcf.gz.tbi",
+        )
 
-        # Workflow Params
-        assert cfg.config.workflow_params.expected_sex_col_name == "Expected_Sex"
-        assert cfg.config.workflow_params.remove_contam is True
-        assert cfg.config.workflow_params.remove_sex_discordant is True
-        assert cfg.config.workflow_params.remove_rep_discordant is True
-        assert cfg.config.workflow_params.remove_unexpected_rep is True
+        assert refs.illumina_manifest_file == Path("test.bpm")
+        assert refs.thousand_genome_vcf == Path("test.vcf.gz")
+        assert refs.thousand_genome_tbi == Path("test.vcf.gz.tbi")
+
+
+def test_user_files_defaults():
+    user_files = UserFiles()
+    assert user_files.gtc_pattern is None
+    assert user_files.idat_pattern is None
+    assert user_files.ped is None
+    assert user_files.map is None
+    assert user_files.bed is None
+    assert user_files.bim is None
+    assert user_files.fam is None
+
+
+def test_software_params_defaults():
+    software_params = SoftwareParams()
+    assert software_params.samp_cr_1 == 0.8
+    assert software_params.snp_cr_1 == 0.8
+    assert software_params.samp_cr_2 == 0.95
+    assert software_params.snp_cr_2 == 0.95
+    assert software_params.ld_prune_r2 == 0.1
+    assert software_params.maf_for_ibd == 0.2
+    assert software_params.ibd_pi_hat_cutoff == 0.95
+    assert software_params.dup_concordance_cutoff == 0.95
+    assert software_params.contam_threshold == 0.2
+    assert software_params.contam_population == "AF"
+    assert software_params.pi_hat_threshold == 0.2
+    assert software_params.autosomal_het_threshold == 0.1
+    assert software_params.minimum_pop_subjects == 50
+    assert software_params.control_hwp_threshold == 50
+    assert software_params.strand == "top"
+
+
+def test_workflow_params_defaults():
+    workflow_params = WorkflowParams()
+    assert workflow_params.expected_sex_col_name == "Expected_Sex"
+    assert workflow_params.remove_contam is True
+    assert workflow_params.remove_sex_discordant is True
+    assert workflow_params.remove_rep_discordant is True
+    assert workflow_params.remove_unexpected_rep is True
+
+
+def test_basic_config(tmp_path):
+    with chdir(tmp_path):
+        [
+            Path(x).touch()
+            for x in ("test.bpm", "test.vcf.gz", "test.vcf.gz.tbi", "sample_sheet.csv")
+        ]
+        cfg = Config(
+            project_name="Test Project",
+            sample_sheet="sample_sheet.csv",
+            reference_files=ReferenceFiles(
+                illumina_manifest_file="test.bpm",
+                thousand_genome_vcf="test.vcf.gz",
+                thousand_genome_tbi="test.vcf.gz.tbi",
+            ),
+            user_files=UserFiles(),
+            software_params=SoftwareParams(),
+            workflow_params=WorkflowParams(),
+        )
+
+        assert cfg.project_name == "Test Project"
+        assert cfg.sample_sheet == Path("sample_sheet.csv")
+        assert cfg.pipeline_version == __version__
 
 
 @pytest.mark.parametrize("value", [-0.01, 0, 1.01])
@@ -82,13 +111,13 @@ def test_call_rate_param_limits(value):
     """
 
     with pytest.raises(ValidationError):
-        cfg_models.SoftwareParams(samp_cr_1=value)
+        SoftwareParams(samp_cr_1=value)
 
     with pytest.raises(ValidationError):
-        cfg_models.SoftwareParams(snp_cr_1=value)
+        SoftwareParams(snp_cr_1=value)
 
     with pytest.raises(ValidationError):
-        cfg_models.SoftwareParams(samp_cr_2=value)
+        SoftwareParams(samp_cr_2=value)
 
     with pytest.raises(ValidationError):
-        cfg_models.SoftwareParams(snp_cr_2=value)
+        SoftwareParams(snp_cr_2=value)
