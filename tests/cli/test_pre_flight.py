d1e3c4da67973d5ac7593ba466a5e22c56aec6d9
Justin Fear <justin.fear@nih.gov>
2020-11-25 10:47:34 -0500

    test: update pre-flight test to work with monkeypatched ConfigMgr

--- tests/cli/test_pre_flight.py
+++ tests/cli/test_pre_flight.py
@@ -1,18 +1,20 @@
 from typer.testing import CliRunner
 
+from cgr_gwas_qc import load_config
 from cgr_gwas_qc.cli.pre_flight import app
 from cgr_gwas_qc.testing import chdir
 
 runner = CliRunner()
 
 
-def test_preflight_refs_only(gtc_working_dir):
-    with chdir(gtc_working_dir):
+def test_preflight_refs_only(small_gtc_working_dir):
+    with chdir(small_gtc_working_dir):
         results = runner.invoke(app, ["--no-gtc-check", "--no-idat-check"])
         assert results.exit_code == 0
 
 
-def test_preflight_refs_and_gtc_only(gtc_working_dir):
-    with chdir(gtc_working_dir):
+def test_preflight_refs_and_gtc_only(small_gtc_working_dir):
+    with chdir(small_gtc_working_dir):
+        load_config()
         results = runner.invoke(app, ["--gtc-check", "--no-idat-check"])
         assert results.exit_code == 0
