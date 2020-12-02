59e4b0df6bec6742354700bcdfddabde1c19742a
Justin Fear <justin.fear@nih.gov>
2020-12-02 12:41:50 -0500

    refactor(config-cli): simplifies code
    I changed the config model to no do file validation. This allows me to simplify this script to no longer use the constrcut method.

--- src/cgr_gwas_qc/cli/config.py
+++ src/cgr_gwas_qc/cli/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import typer
 
 from cgr_gwas_qc.config import config_to_yaml
-from cgr_gwas_qc.models import config as cfg_models
+from cgr_gwas_qc.models.config import Config
 
 app = typer.Typer(add_completion=False)
 
@@ -27,47 +27,41 @@ def main(
 
 
 def cgems_config(project_name, sample_sheet):
-    return cfg_models.Config.construct(
+    return Config(
         project_name=project_name,
         sample_sheet=sample_sheet,
-        reference_files=cfg_models.ReferenceFiles.construct(
+        reference_files=dict(
             illumina_manifest_file="/DCEG/CGF/Infinium/Resources/Manifests/GSAMD-Files/build37/GSAMD-24v1-0_20011747_A1.bpm",
             thousand_genome_vcf="/DCEG/CGF/Bioinformatics/Production/data/thousG/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.vcf.gz",
             thousand_genome_tbi="/DCEG/CGF/Bioinformatics/Production/data/thousG/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.vcf.gz.tbi",
         ),
-        user_files=cfg_models.UserFiles.construct(
-            idat_pattern=cfg_models.Idat.construct(
+        user_files=dict(
+            idat_pattern=dict(
                 red="/DCEG/CGF/Infinium/ScanData/CGF/ByProject/{Project}/{SentrixBarcode_A}/{SentrixBarcode_A}_{SentrixPosition_A}_Red.idat",
                 green="/DCEG/CGF/Infinium/ScanData/CGF/ByProject/{Project}/{SentrixBarcode_A}/{SentrixBarcode_A}_{SentrixPosition_A}_Grn.idat",
             ),
             gtc_pattern="/DCEG/CGF/Infinium/ScanData/CGF/ByProject/{Project}/{SentrixBarcode_A}/{SentrixBarcode_A}_{SentrixPosition_A}.gtc",
         ),
-        software_params=cfg_models.SoftwareParams(),
-        workflow_params=cfg_models.WorkflowParams(),
-        env_modules=cfg_models.EnvModules.construct(
-            plink2="plink2/1.90b5", eigensoft="eigensoft/7.2.1", r="R/3.4.0",
-        ),
+        env_modules=dict(plink2="plink2/1.90b5", eigensoft="eigensoft/7.2.1", r="R/3.4.0",),
     )
 
 
 def general_config(project_name, sample_sheet):
-    return cfg_models.Config.construct(
+    return Config(
         project_name=project_name,
         sample_sheet=sample_sheet,
-        reference_files=cfg_models.ReferenceFiles.construct(
+        reference_files=dict(
             illumina_manifest_file="/path/to/illumina/manifest.bpm",
             thousand_genome_vcf="/path/to/thousand/genome/vcf.gz",
             thousand_genome_tbi="/path/to/thousand/genome/vcf.gz.tbi",
         ),
-        user_files=cfg_models.UserFiles.construct(
-            idat_pattern=cfg_models.Idat.construct(
+        user_files=dict(
+            idat_pattern=dict(
                 red="/expample/pattern/wildcards/are/columns/in/sample_sheet/{Project}/{Sample_ID}}_Red.idat",
                 green="/expample/pattern/wildcards/are/columns/in/sample_sheet/{Project}/{Sample_ID}}_Grn.idat",
             ),
             gtc_pattern="/expample/pattern/wildcards/are/columns/in/sample_sheet/{Project}/{Sample_ID}.gtc",
         ),
-        software_params=cfg_models.SoftwareParams(),
-        workflow_params=cfg_models.WorkflowParams(),
     )
 
 
