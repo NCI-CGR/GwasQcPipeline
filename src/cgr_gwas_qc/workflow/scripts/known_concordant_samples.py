b206a7852b6a4b49e1cce9b9586941a13f80fec8
Justin Fear <justin.fear@nih.gov>
2020-12-11 10:13:33 -0500

    feat(workflow/scripts): add spliting of qc and study samples
    Adds logic to split the known concordant samples into Internal QC and Study samples. Also does some general code clean up.

--- src/cgr_gwas_qc/workflow/scripts/known_concordant_samples.py
+++ src/cgr_gwas_qc/workflow/scripts/known_concordant_samples.py
@@ -1,3 +1,14 @@
+"""Create tables of sample concordance.
+
+This script uses sample concordance based on pairwise IBD estimations. We
+expect samples from the same subject to show high concordance while samples
+from different subjects should not be related. This script outputs 4 tables:
+
+- Known concordant samples (Full Table)
+- Known concordant samples (QC Samples Only)
+- Known concordant samples (Study Samples Only)
+- Unknown concordant samples (Full Table)
+"""
 from pathlib import Path
 from typing import Optional
 
@@ -8,8 +19,6 @@ from cgr_gwas_qc.parsers.sample_sheet import SampleSheet
 
 app = typer.Typer(add_completion=False)
 
-minConcordance = 1.0
-
 
 @app.command()
 def main(
@@ -18,30 +27,54 @@ def main(
         ..., help="Path to the `plink_filter_call_rate_2/samples.imiss`"
     ),
     ibd_file: Path = typer.Argument(..., help="Path to the `ibd/samples.genome` file."),
-    dup_concordance_cutoff: float = typer.Argument(
-        ..., help="The concordance threshold for samples from different subjects."
-    ),
-    known_file: Path = typer.Argument(..., help="Path to save known concordant samples."),
-    unknown_file: Path = typer.Argument(..., help="Path to save unknown condorant samples."),
     subject_id_override: Optional[str] = typer.Argument(
         None,
         help="Specify which column corresponds to subjects. [Deprecated: Use the `Group_By` column sample sheet]",
     ),
+    dup_concordance_cutoff: float = typer.Argument(
+        ..., help="The concordance threshold for samples from different subjects."
+    ),
+    known_file: Path = typer.Argument(..., help="Path to save known concordant samples."),
+    known_internal_qc: Path = typer.Argument(
+        ..., help="Path to save known concordant internal QC samples."
+    ),
+    known_study_samples: Path = typer.Argument(
+        ..., help="Path to save known concordant study samples."
+    ),
+    unknown_file: Path = typer.Argument(..., help="Path to save unknown condorant samples."),
 ):
-    sample_metadata = read_sample_metadata(sample_sheet, imiss_file, subject_id_override)
+    ################################################################################
+    # Pairwise sample concordance + sample metadata
+    ################################################################################
     sample_concordance = read_pairwise_sample_concordance(ibd_file)
+    sample_metadata = read_sample_metadata(sample_sheet, imiss_file, subject_id_override)
 
-    # Merge on sample metadata for Sample 1 and Sample 2 in the concordance
-    # comparison. Add suffix to metadata columns.
+    # Add metadata for Sample 1 and Sample 2 in pairwise table
     df = sample_concordance.merge(
         sample_metadata.add_suffix("1"), on="Sample_ID1", how="left"
     ).merge(sample_metadata.add_suffix("2"), on="Sample_ID2", how="left")
 
-    # Save concordance information for samples from the same subject
-    create_known_concordant_table(df).to_csv(known_file, index=False)
+    ################################################################################
+    # Save Known Concordant Samples
+    # (i.e., samples from the same subject)
+    ################################################################################
+    # Full Table
+    known_df = create_known_concordant_table(df)
+    known_df.to_csv(known_file, index=False)  # Full table
 
-    # Save a list of samples from different subjects but with high concordance
-    create_unknown_concordant_table(df, dup_concordance_cutoff).to_csv(unknown_file, index=False)
+    # QC samples only
+    qc_subjects = sample_metadata.query("Sample_Group == 'sVALD-001'").Subject_ID.values  # noqa
+    known_df.query("Subject_ID in @qc_subjects").to_csv(known_internal_qc, index=False)
+
+    # Study samples only
+    known_df.query("Subject_ID not in @qc_subjects").to_csv(known_study_samples, index=False)
+
+    ################################################################################
+    # Save Unknown Concordant Samples
+    # (i.e., highly concordant samples from different subjects)
+    ################################################################################
+    unknown_df = create_unknown_concordant_table(df, dup_concordance_cutoff)
+    unknown_df.to_csv(unknown_file, index=False)
 
 
 def read_sample_metadata(
@@ -50,14 +83,14 @@ def read_sample_metadata(
     """Read in sample metadata from sample sheet and call rates.
 
     Returns:
-        DataFrame with ["Sample_ID", "Subject_ID", "call_rate"]
+        DataFrame with ["Sample_ID", "Subject_ID", "Sample_Group", "call_rate"]
     """
     return (
         SampleSheet(sample_sheet)
         .add_group_by_column(subject_id_override)
         .data.merge(read_imiss_file(call_rate), how="left")
         .rename({"Group_By_Subject_ID": "Subject_ID"}, axis=1)
-        .loc[:, ("Sample_ID", "Subject_ID", "call_rate")]
+        .loc[:, ("Sample_ID", "Subject_ID", "Sample_Group", "call_rate")]
     )
 
 
@@ -98,6 +131,10 @@ def create_known_concordant_table(df: pd.DataFrame) -> pd.DataFrame:
       min concordance)
     - If either sample missing call rate then set (PI_HAT and concordance to
       Nan)
+
+    Returns:
+        DataFrame with ["Subject_ID", "Sample_ID1", "Sample_ID2",
+          "concordance", "PI_HAT"]
     """
     _df = df.copy()
 
@@ -123,6 +160,10 @@ def create_unknown_concordant_table(
 
     1. Samples have different subject IDs
     2. Sample concordance is > `dup_concordance_cutoff`
+
+    Returns:
+        DataFrame with ["Subject_ID1", "Subject_ID2", "Sample_ID1",
+        "Sample_ID2", "concordance", "PI_HAT"]
     """
     return df.query("Subject_ID1 != Subject_ID2 & concordance > @dup_concordance_cutoff").loc[
         :, ("Subject_ID1", "Subject_ID2", "Sample_ID1", "Sample_ID2", "concordance", "PI_HAT")
@@ -132,13 +173,15 @@ def create_unknown_concordant_table(
 if __name__ == "__main__":
     if "snakemake" in locals():
         main(
-            snakemake.input.sampleSheet,  # type: ignore # noqa
-            snakemake.input.imiss3,  # type: ignore # noqa
+            snakemake.input.sample_sheet,  # type: ignore # noqa
+            snakemake.input.imiss,  # type: ignore # noqa
             snakemake.input.ibd,  # type: ignore # noqa
+            snakemake.params.subject_id_override,  # type: ignore # noqa
             snakemake.params.concordance_threshold,  # type: ignore # noqa
             snakemake.output.known,  # type: ignore # noqa
+            snakemake.output.known_qc,  # type: ignore # noqa
+            snakemake.output.known_study,  # type: ignore # noqa
             snakemake.output.unknown,  # type: ignore # noqa
-            snakemake.params.subject_id_override,  # type: ignore # noqa
         )
     else:
         app()
