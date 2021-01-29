160b41d856576f5adfdf95e29932fcacaad31f1e
Justin Fear <justin.fear@nih.gov>
2021-01-29 08:00:27 -0500

    refactor(subject_qc): refactor to make code clearer
    Some small changes trying to make the code/comments clearer.

--- src/cgr_gwas_qc/workflow/scripts/related_subjects.py
+++ src/cgr_gwas_qc/workflow/scripts/related_subjects.py
@@ -17,27 +17,28 @@ def main(
     pi_hat_threshold: float = typer.Argument(  # noqa
         ..., help="The threshold to consider subjects related."
     ),
-    out_file: Path = typer.Argument(..., help="Path to save list subjects to remove."),
+    out_file: Path = typer.Argument(..., help="Path to save a list of subjects to remove."),
 ):
-    """Create related subjects pruning list.
+    """Create a related subjects pruning list.
 
-    The goal of this script is to prune related subjects based on PI_HAT. It
-    uses a greedy based graph algorithm to select which subjects should be
-    pruned. It does the following:
+    The goal of this script is to prune related subjects based on `PI_HAT`.
+    It uses a graph based algorithm to select which subjects should be
+    pruned. It does this by doing the following:
 
-    1. Uses PI_HAT from IBS/IBD estimation to identify pairwise groups of
+    1. Uses `PI_HAT` from IBS/IBD estimation to identify pairwise groups of
        related subjects.
     2. Builds a graph where subjects (nodes) are connected by edges if their
-       PI_HAT was above the pi_hat_threshold.
+       `PI_HAT` was above the pi_hat_threshold.
     3. Identifies subjects that have the most relatives (ie., nodes with max
        degree).
-    4. Randomly selects one of these subjects for removal.
+    4. Randomly selects and removes one of these highly connected subjects.
+       Adds this subject to the output pruning list.
     5. Removes any subjects that no longer have a relative in the graph
        (i.e., isolated nodes with no edges).
     6. Repeats 3-5 until there are no subjects left in the graph.
 
-    Writes out a list of subjects to prune in a format compatible with
-    PLINK's `--remove` option.
+    Saves a list of subjects to prune in a format compatible with PLINK's
+    `--remove` option.
     """
     pairwise_related_subjects = list(
         pd.read_csv(ibs, delim_whitespace=True)
@@ -51,11 +52,12 @@ def main(
         out_file.touch()
         return None
 
-    # Create a graph with subjects as nodes.
+    # Create a graph where subjects are nodes and edges are relatedness at a
+    # `PI_HAT > pi_hat_threshold`.
     G = nx.Graph()
     G.add_edges_from(pairwise_related_subjects)
 
-    # Create pruning list and save.
+    # Create a pruning list by removing the most connected subjects first.
     prune_list = create_prune_list(G)
     out_file.write_text("\n".join(prune_list))
 
@@ -70,28 +72,29 @@ def create_prune_list(
     Args:
         G: A graph where nodes are subjects and edges indicate two
           subjects are related.
-        seed: A numpy random seed. This is required for testing.
+        seed: A numpy random seed. Added to allow tests to have consistent
+          output.
 
     Yields:
-        Generator[str, None, None]: A string in the format "Subject_ID Subject_ID"
+        Generator: A string in the format "Subject_ID Subject_ID"
     """
     while len(G) > 0:
         yield from _prune(G, seed)
 
 
 def _prune(G: nx.Graph, seed: Optional[RandomState]) -> Generator[str, None, None]:
-    node_to_remove = (
+    subject_to_remove = (
         pd.Series(dict(G.degree))
         .pipe(lambda x: x[x == x.max()])  # select the subjects with the most relatives
-        .sample(n=1, random_state=seed)  # randomly select one of these subjects
+        .sample(n=1, random_state=seed)  # randomly select one of these highly connect subjects
         .index[0]
     )
-    G.remove_node(node_to_remove)  # remove this subject
+    G.remove_node(subject_to_remove)  # remove selected subject from the graph
 
     non_connected_nodes = list(nx.isolates(G))  # Find subjects with no remaining relatives
-    G.remove_nodes_from(non_connected_nodes)  # Remove these isolated subjects from the graph
+    G.remove_nodes_from(non_connected_nodes)  # Remove isolated subjects from the graph
 
-    yield f"{node_to_remove} {node_to_remove}"  # add node_to_remove to pruning list
+    yield f"{subject_to_remove} {subject_to_remove}"  # add subject_to_remove to pruning list
 
 
 if __name__ == "__main__":
