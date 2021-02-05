1103b5382f5fa4edc61270c533e3880d583a9d02
Justin Fear <justin.m.fear@gmail.com>
2021-02-05 17:20:18 -0500

    test: updates bim tests to match changes to bim record

--- tests/parsers/test_bim.py
+++ tests/parsers/test_bim.py
@@ -6,54 +6,50 @@ import pytest
 from cgr_gwas_qc.parsers import bim
 
 
-def test_bim_reader():
-    bim_file = dedent(
-        # chrom id morgan coord allele1 allele2
-        """\
-        1  rs123  0  123  A  G
-        1  rs128  0  128  T  G
-        """
-    )
-
-    expected_records = [
-        bim.BimRecord("1", "rs123", 0, 123, "A", "G"),
-        bim.BimRecord("1", "rs128", 0, 128, "T", "G"),
-    ]
-
-    with bim.open(StringIO(bim_file), "r") as fh:
-        records = [record for record in fh]
-
-    assert expected_records == records
-
-
-def test_bim_writer(tmp_path):
-    records = [
-        bim.BimRecord("1", "rs123", 0, 123, "A", "G"),
-        bim.BimRecord("1", "rs128", 0, 128, "T", "G"),
-    ]
-
-    expected = dedent(
+@pytest.fixture(scope="module")
+def bim_file():
+    return dedent(
         # chrom id morgan coord allele1 allele2
         """\
         1 rs123 0 123 A G
         1 rs128 0 128 T G
+        23 rs129 0 129 T G
         """
     )
+
+
+@pytest.fixture(scope="module")
+def bim_records():
+    return [
+        bim.BimRecord("rs123", "1", 123, "A", "G", "1", 0),
+        bim.BimRecord("rs128", "1", 128, "T", "G", "1", 0),
+        bim.BimRecord("rs129", "X", 129, "T", "G", "23", 0),
+    ]
+
+
+def test_bim_reader(bim_file, bim_records):
+    with bim.open(StringIO(bim_file), "r") as fh:
+        records = [record for record in fh]
+
+    assert bim_records == records
+
+
+def test_bim_writer(bim_file, bim_records, tmp_path):
     out_file = tmp_path / "test.bim"
 
     with bim.open(out_file, "w") as fh:
-        [fh.write(record) for record in records]
+        [fh.write(record) for record in bim_records]
 
-    assert expected == out_file.read_text()
+    assert bim_file == out_file.read_text()
 
 
 ambiguous = [
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "T"), True),
-    (bim.BimRecord("1", "rs123", 0, 123, "G", "C"), True),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "C"), False),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "G"), False),
-    (bim.BimRecord("1", "rs123", 0, 123, "T", "C"), False),
-    (bim.BimRecord("1", "rs123", 0, 123, "T", "G"), False),
+    (bim.BimRecord("rs123", "1", 123, "A", "T"), True),
+    (bim.BimRecord("rs123", "1", 123, "G", "C"), True),
+    (bim.BimRecord("rs123", "1", 123, "A", "C"), False),
+    (bim.BimRecord("rs123", "1", 123, "A", "G"), False),
+    (bim.BimRecord("rs123", "1", 123, "T", "C"), False),
+    (bim.BimRecord("rs123", "1", 123, "T", "G"), False),
 ]
 
 
@@ -63,10 +59,10 @@ def test_is_ambiguous(record, expected):
 
 
 indel = [
-    (bim.BimRecord("1", "rs123", 0, 123, "I", "D"), True),
-    (bim.BimRecord("1", "rs123", 0, 123, "I", "C"), True),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "D"), True),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "G"), False),
+    (bim.BimRecord("rs123", "1", 123, "I", "D"), True),
+    (bim.BimRecord("rs123", "1", 123, "I", "C"), True),
+    (bim.BimRecord("rs123", "1", 123, "A", "D"), True),
+    (bim.BimRecord("rs123", "1", 123, "A", "G"), False),
 ]
 
 
@@ -76,12 +72,12 @@ def test_is_indel(record, expected):
 
 
 minor_chrom = [
-    (bim.BimRecord("0", "rs123", 0, 123, "A", "G"), True),
-    (bim.BimRecord("24", "rs123", 0, 123, "A", "G"), True),
-    (bim.BimRecord("25", "rs123", 0, 123, "A", "G"), True),
-    (bim.BimRecord("26", "rs123", 0, 123, "A", "G"), True),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "G"), False),
-    (bim.BimRecord("23", "rs123", 0, 123, "A", "G"), False),
+    (bim.BimRecord("rs123", "0", 123, "A", "G"), True),
+    (bim.BimRecord("rs123", "24", 123, "A", "G"), True),
+    (bim.BimRecord("rs123", "25", 123, "A", "G"), True),
+    (bim.BimRecord("rs123", "26", 123, "A", "G"), True),
+    (bim.BimRecord("rs123", "1", 123, "A", "G"), False),
+    (bim.BimRecord("rs123", "X", 123, "A", "G"), False),
 ]
 
 
@@ -91,11 +87,11 @@ def test_not_major_chrom(record, expected):
 
 
 allele_order = [
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "T"), ("T", "A")),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "C"), ("C", "A")),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "G"), ("G", "A")),
-    (bim.BimRecord("1", "rs123", 0, 123, "T", "C"), ("C", "T")),
-    (bim.BimRecord("1", "rs123", 0, 123, "T", "G"), ("G", "T")),
+    (bim.BimRecord("rs123", "1", 123, "A", "T"), ("T", "A")),
+    (bim.BimRecord("rs123", "1", 123, "A", "C"), ("C", "A")),
+    (bim.BimRecord("rs123", "1", 123, "A", "G"), ("G", "A")),
+    (bim.BimRecord("rs123", "1", 123, "T", "C"), ("C", "T")),
+    (bim.BimRecord("rs123", "1", 123, "T", "G"), ("G", "T")),
 ]
 
 
@@ -106,11 +102,11 @@ def test_switch_allele_order(record, expected):
 
 
 complements = [
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "T"), ("T", "A")),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "C"), ("T", "G")),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "G"), ("T", "C")),
-    (bim.BimRecord("1", "rs123", 0, 123, "I", "G"), ("I", "C")),
-    (bim.BimRecord("1", "rs123", 0, 123, "D", "G"), ("D", "C")),
+    (bim.BimRecord("rs123", "1", 123, "A", "T"), ("T", "A")),
+    (bim.BimRecord("rs123", "1", 123, "A", "C"), ("T", "G")),
+    (bim.BimRecord("rs123", "1", 123, "A", "G"), ("T", "C")),
+    (bim.BimRecord("rs123", "1", 123, "I", "G"), ("I", "C")),
+    (bim.BimRecord("rs123", "1", 123, "D", "G"), ("D", "C")),
 ]
 
 
@@ -121,11 +117,11 @@ def test_complement_alleles(record, expected):
 
 
 problems = [
-    (bim.BimRecord("0", "rs123", 0, 123, "A", "C"), ["not_major_chrom"]),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "T"), ["ambiguous_allele"]),
-    (bim.BimRecord("1", "rs123", 0, 123, "A", "I"), ["indel"]),
-    (bim.BimRecord("1", "rs123", 0, 0, "A", "C"), ["bad_position"]),
-    (bim.BimRecord("1", "rs123", 0, 0, "A", "T"), ["ambiguous_allele", "bad_position"]),
+    (bim.BimRecord("rs123", "0", 123, "A", "C"), ["not_major_chrom"]),
+    (bim.BimRecord("rs123", "1", 123, "A", "T"), ["ambiguous_allele"]),
+    (bim.BimRecord("rs123", "1", 123, "A", "I"), ["indel"]),
+    (bim.BimRecord("rs123", "1", 0, "A", "C"), ["bad_position"]),
+    (bim.BimRecord("rs123", "1", 0, "A", "T"), ["ambiguous_allele", "bad_position"]),
 ]
 
 
