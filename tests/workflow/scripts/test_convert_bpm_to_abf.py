921d1f343469464e47c42a9598ebc8053e539b71
Justin Fear <justin.m.fear@gmail.com>
2021-02-05 16:21:58 -0500

    test(parsers): add unit tests and remove real data end-to-end tests because too slow

--- tests/workflow/scripts/test_convert_bpm_to_abf.py
+++ tests/workflow/scripts/test_convert_bpm_to_abf.py
@@ -4,34 +4,16 @@ For contamination checking we need to have the population level allele
 frequencies. We pull these allele frequencies from the 1KG project for each
 SNP in the BPM.
 """
-import warnings
 from pathlib import Path
 
 import pandas as pd
 import pytest
 from typer.testing import CliRunner
 
-from cgr_gwas_qc.testing.data import RealData
+from cgr_gwas_qc.parsers import bpm, vcf
 from cgr_gwas_qc.workflow.scripts.bpm2abf import app
 
 runner = CliRunner()
-
-
-################################################################################
-# Compare to outputs from old script.
-################################################################################
-@pytest.mark.regression
-def test_bpm2abf_AF(bpm_file, vcf_file, tmp_path):
-    file_out = tmp_path / bpm_file.with_suffix(".abf.txt").name
-    results = runner.invoke(
-        app, [bpm_file.as_posix(), vcf_file.as_posix(), "AF", file_out.as_posix()]
-    )
-    assert results.exit_code == 0
-
-    obs_df = pd.read_csv(file_out, sep="\t").fillna(0)  # legacy uses 0.0 instead of NA.
-    exp_df = pd.read_csv("tests/data/small_manifest.AF.abf.txt", sep="\t")
-    prop_very_different = (abs(obs_df.ABF - exp_df.ABF) > 1e-6).mean()
-    assert 0.01 > prop_very_different  # less than 1% are more than 0.000001 different
 
 
 ################################################################################
@@ -52,66 +34,83 @@ def test_bpm2abf_unknown_population(bpm_file, vcf_file, tmpdir):
 
 
 ################################################################################
-# GitLab Issue #30 failed regression test with real data.
+# Check main logic
 ################################################################################
-@pytest.mark.real_data
-@pytest.mark.regression
-def test_bpm2abf_real_data(tmp_path):
-    # GIVEN: A real Illumina BPM and 1KG vcf file
-    data_store = RealData()
-    bpm_file = (data_store / data_store._illumina_manifest_file).as_posix()
-    vcf_file = (data_store / data_store._thousand_genome_vcf).as_posix()
-    exp_abf = data_store / "production_outputs/GSAMD-24v1-0_20011747_A1.AF.abf.txt"
-    obs_abf = tmp_path / "test.abf.txt"
+bpm_vcf_records = [
+    pytest.param(  # Perfect Match
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [vcf.VcfRecord("rs124", "1", 100, "A", ("G",), None, {"AF": (0.1,)})],
+        0.1,
+        id="Perfect Match",
+    ),
+    pytest.param(
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [vcf.VcfRecord("rs124", "1", 100, "G", ("A",), None, {"AF": (0.1,)})],
+        0.9,
+        id="Alleles Flipped",
+    ),
+    pytest.param(
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [vcf.VcfRecord("rs124", "1", 100, "G", ("A", "T"), None, {"AF": (0.1, 0.2)})],
+        "NA",
+        id="Multi-allelic",
+    ),
+    pytest.param(
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [vcf.VcfRecord("rs124", "1", 100, "G", ("ACGA",), None, {"AF": (0.1,)})],
+        "NA",
+        id="Indel",
+    ),
+    pytest.param(
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [vcf.VcfRecord("rs124", "1", 101, "A", ("G",), None, {"AF": (0.1,)})],
+        "NA",
+        id="Wrong position",
+    ),
+    pytest.param(
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [
+            vcf.VcfRecord("rs124", "1", 100, "A", ("ACGA",), None, {"AF": (0.25,)}),
+            vcf.VcfRecord("rs124", "1", 100, "A", ("G",), None, {"AF": (0.1,)}),
+        ],
+        0.1,
+        id="Indel and Perfect Match",
+    ),
+    pytest.param(
+        bpm.BpmRecord("rs123", "1", 100, "A", "G"),
+        [
+            vcf.VcfRecord("rs124", "1", 100, "A", ("G",), None, {"AF": (0.1,)}),
+            vcf.VcfRecord("rs124", "1", 100, "A", ("G",), None, {"AF": (0.7,)}),
+        ],
+        0.1,
+        id="Two Perfect Matches only use first",
+    ),
+]
 
-    # WHEN: I run the script using the allele frequencies from "AF"
-    results = runner.invoke(
-        app, [bpm_file, vcf_file, "AF", obs_abf.as_posix()], catch_exceptions=False
-    )
-    assert results.exit_code == 0
 
-    # THEN: I expect the results to match the legacy workflow closely.
-    # NOTE: There is a bug in the legacy workflow where they use ABF from
-    # indels. So I expect most markers to be close but some to be very
-    # different.
-    obs_df = pd.read_csv(obs_abf, sep="\t").fillna(0)  # legacy uses 0.0 instead of NA.
-    exp_df = pd.read_csv(exp_abf, sep="\t")
-    prop_very_different = (abs(obs_df.ABF - exp_df.ABF) > 1e-6).mean()
-    assert 0.01 > prop_very_different  # less than 1% are more than 0.000001 different
-
-
-@pytest.mark.parametrize("pos", [-10, 0], ids=["negative", "zero"])
-def test_bpm2abf_impossible_positions(vcf_file, pos):
-    from cgr_gwas_qc.parsers import bpm, vcf
+@pytest.mark.parametrize("bpm_record,vcf_records,AF", bpm_vcf_records)
+def test_get_abf_from_vcf(bpm_record, vcf_records, AF):
     from cgr_gwas_qc.workflow.scripts.bpm2abf import get_abf_from_vcf
 
-    # GIVEN: The 1KG VCF and a variant with and impossible position.
-    record = bpm.BpmRecord("rs123", "[A/C]", "1", pos, 1, 1)
+    class VcfFileMock:
+        def fetch(*args, **kwargs):
+            return vcf_records
 
-    with vcf.open(vcf_file) as vcf_fh:
-        # WHEN: I try to look up the variant in the VCF
-        res = get_abf_from_vcf(record, vcf_fh, "AF")
-
-    # THEN: I get no exceptions and I return None
-    assert "NA" == res
+    assert AF == get_abf_from_vcf(bpm_record, VcfFileMock(), "AF")
 
 
 ################################################################################
-# GRCh38 Support
+# Compare to outputs from old script.
 ################################################################################
-@pytest.mark.real_data
-def test_bpm2abf_GRCh38_AF(tmp_path):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        data_cache = RealData(GRCh_version=38)
-
-    bpm_file = data_cache / data_cache._illumina_manifest_file
-    vcf_file = data_cache / data_cache._thousand_genome_vcf
+@pytest.mark.regression
+def test_bpm2abf_AF(bpm_file, vcf_file, tmp_path):
     file_out = tmp_path / bpm_file.with_suffix(".abf.txt").name
     results = runner.invoke(
         app, [bpm_file.as_posix(), vcf_file.as_posix(), "AF", file_out.as_posix()]
     )
     assert results.exit_code == 0
 
-    obs_abf = pd.read_csv(file_out, sep="\t")
-    assert obs_abf.notna().mean().ABF >= 0.90  # Make sure >=90% of SNPs are not missing
+    obs_df = pd.read_csv(file_out, sep="\t").fillna(0)  # legacy uses 0.0 instead of NA.
+    exp_df = pd.read_csv("tests/data/small_manifest.AF.abf.txt", sep="\t")
+    prop_very_different = (abs(obs_df.ABF - exp_df.ABF) > 1e-6).mean()
+    assert 0.01 > prop_very_different  # less than 1% are more than 0.000001 different
