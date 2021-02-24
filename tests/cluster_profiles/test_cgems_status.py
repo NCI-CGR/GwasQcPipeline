d7d6b661f69bda415bdb928c2fc29d0dade98dfe
Justin Fear <justin.m.fear@gmail.com>
2021-02-24 16:07:27 -0500

    test(submission): fix tests to work with submission changes

--- tests/cluster_profiles/test_cgems_status.py
+++ tests/cluster_profiles/test_cgems_status.py
@@ -6,8 +6,3 @@ from cgr_gwas_qc.cluster_profiles.cgems import cgems_status
 @pytest.mark.parametrize("job_id,expected_status", [(2, "running"), (4, "failed"), (22, None)])
 def test_check_queue(qsub, job_id, expected_status):
     assert expected_status == cgems_status.check_queue(job_id)
-
-
-@pytest.mark.parametrize("job_id,expected_status", [(1, "success"), (0, "failed")])
-def test_check_job_history(qsub, job_id, expected_status):
-    assert expected_status == cgems_status.check_job_history(job_id)
