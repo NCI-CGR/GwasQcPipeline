b3642d6608876abf536d80ccea349516ba96922d
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-03-01 09:02:47 -0500

    feat(submission): add better error handling to main process submission script

--- src/cgr_gwas_qc/cluster_profiles/snakemake.sh
+++ src/cgr_gwas_qc/cluster_profiles/snakemake.sh
@@ -13,8 +13,13 @@
 {% if local_tasks > 1 %}
 #$ -pe by_node {{ local_tasks }}
 {% endif %}
+
 export PATH=$CONDA_PREFIX/bin:$(dirname $CONDA_EXE):$PATH
-source /etc/profile.d/modules.sh; module load sge; unset module
+source /etc/profile.d/modules.sh
+module load sge
+unset module
+
+CLUSTER_JOB_ID=${JOB_ID}
 {% endif %}
 {% if biowulf %}
 #SBATCH --job-name="GwasQcPipeline"
@@ -25,19 +30,47 @@ source /etc/profile.d/modules.sh; module load sge; unset module
 #SBATCH --ntasks={{ local_tasks }}
 #SBATCH --cpus-per-task=1
 #SBATCH --mem={{ local_mem_mb }}
-JOB_ID=${SLURM_JOB_ID}
+
+CLUSTER_JOB_ID=${SLURM_JOB_ID}
 {% endif %}
 
-set -euo pipefail
+shopt -s -o errexit pipefail nounset
+
+cgr_exit_message() {
+    printf "\n\n"
+    printf "################################################################################\n"
+    if [[ $? != 0 ]]; then
+        printf "# CGR SUBMIT: There was an error running the workflow\n"
+    else
+        printf "# CGR SUBMIT: Workflow complete\n"
+    fi
+    printf "################################################################################\n"
+    printf "\n\n"
+}
+trap cgr_exit_message EXIT
 
 cd {{ working_dir }}
 [[ -d logs ]] || mkdir -p logs
 
-{{ python_executable }} -m cgr_gwas_qc snakemake --local-cores {{ local_tasks }} --profile {{ profile }} {{ group_options }}
-
-PCT_DONE=$(grep -B1 "Complete log" gwas_qc_log.$JOB_ID | sed -nr "s/.*\((.*)\%\).*/\1/p")
-
-if [ $PCT_DONE -ne 100 ]; then
-    echo "CGR SUBMIT: Re-Starting workflow to finish incomplete tasks"
+# Run the workflow
+run_workflow() {
     {{ python_executable }} -m cgr_gwas_qc snakemake --local-cores {{ local_tasks }} --profile {{ profile }} {{ group_options }}
+}
+
+run_workflow
+
+# Check the log and make sure everything completed as expected
+sleep 10  # in case of file latency
+PCT_DONE=$(tail -n 5 gwas_qc_log.$CLUSTER_JOB_ID | sed -nr "s/.*[[:digit:]]+ of [[:digit:]]+ steps \((.*)\%\) done.*/\1/p")
+
+if [[ $PCT_DONE != "" && $PCT_DONE != 100 ]]; then
+    # The pipeline ran successfully but ended early so restart
+    printf "\n\n"
+    printf "################################################################################\n"
+    printf "# CGR SUBMIT: Re-Starting workflow to finish incomplete tasks\n"
+    printf "################################################################################\n"
+    printf "\n\n"
+    run_workflow
 fi
+
+exit 0
