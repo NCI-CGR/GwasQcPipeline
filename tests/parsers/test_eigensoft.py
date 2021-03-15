aa72ecd4c87e7a581b36b5a210fc69a360c2621c
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-03-15 13:45:14 -0400

    test: mark eigensoft tests as using real data

--- tests/parsers/test_eigensoft.py
+++ tests/parsers/test_eigensoft.py
@@ -5,12 +5,14 @@ from cgr_gwas_qc.parsers.eigensoft import Eigenvec
 from cgr_gwas_qc.testing.data import RealData
 
 
+@pytest.mark.real_data
 @pytest.fixture(scope="module")
 def eigenvec():
     filename = RealData() / "production_outputs/pca/EUR_subjects.eigenvec"
     return Eigenvec(filename)
 
 
+@pytest.mark.real_data
 def test_Eigenvec_componenets(eigenvec: Eigenvec):
     cols = ["PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10"]
     assert "ID" == eigenvec.components.index.name
@@ -18,6 +20,7 @@ def test_Eigenvec_componenets(eigenvec: Eigenvec):
     assert all(eigenvec.components.dtypes[1:] == np.float64)  # all PCs should be floats
 
 
+@pytest.mark.real_data
 def test_Eigenvec_values(eigenvec: Eigenvec):
     assert eigenvec.values.shape[0] == 10
     assert eigenvec.values.dtype == np.float64
