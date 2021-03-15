4d4223f7029e68ef8f81fb3dacaab93217b9ff93
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-03-15 14:21:41 -0400

    test: mark plink test with real data flag

--- tests/parsers/test_plink.py
+++ tests/parsers/test_plink.py
@@ -1,7 +1,10 @@
+import pytest
+
 from cgr_gwas_qc.parsers import plink
 from cgr_gwas_qc.testing.data import RealData
 
 
+@pytest.mark.real_data
 def test_read_het():
     filename = RealData() / "production_outputs/plink_start/samples_start.het"
     df = plink.read_het(filename)
