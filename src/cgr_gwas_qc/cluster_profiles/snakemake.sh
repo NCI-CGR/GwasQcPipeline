eba790a5fef08f93c3ddc5622bd5037ef7100603
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-03-16 11:00:37 -0400

    feat(submission): add more advanced signal handling in main job submission script
    Add trapping of cluster kill events and fixes exit status messaging
    
    Closes #73, closes #103

--- src/cgr_gwas_qc/cluster_profiles/snakemake.sh
+++ src/cgr_gwas_qc/cluster_profiles/snakemake.sh
@@ -5,6 +5,7 @@
 #$ -v CONDA_EXE,CONDA_PREFIX
 #$ -cwd
 #$ -terse
+#$ -notify
 #$ -j yes
 #$ -o gwas_qc_log.$JOB_ID
 #$ -q {{ queue }}
@@ -34,42 +35,73 @@ CLUSTER_JOB_ID=${SLURM_JOB_ID}
 
 shopt -s -o errexit pipefail nounset
 
+CLUSTER_KILLED=0
+
+UNICORN="\360\237\246\204"
+PENGUIN="\xF0\x9F\x90\xA7"
+DNA="\xF0\x9F\xA7\xAC"
+
+cgr_bar(){
+    printf "################################################################################\n"
+}
+
 cgr_get_time() {
     date +'%H:%M:%S %Y-%m-%d'
 }
 
 cgr_start_message() {
-    printf "################################################################################\n"
+    cgr_bar
     printf "# CGR SUBMIT: Starting GWAS QC Workflow\n"
     printf "# User: %s\n" $USER
     printf "# Version: {{ version }}\n"
     printf "# Profile: {{ profile }}\n"
     printf "# Snakefile: {{ snakefile }}\n"
     printf "# Start Time: %s\n# Date: %s\n" $(cgr_get_time)
-    printf "################################################################################\n"
+    cgr_bar
 }
 
 cgr_restart_message() {
     printf "\n"
-    printf "################################################################################\n"
+    cgr_bar
     printf "# CGR SUBMIT: Re-Starting workflow to finish incomplete tasks\n"
     printf "# Start Time: %s\n# Date: %s\n" $(cgr_get_time)
-    printf "################################################################################\n"
+    cgr_bar
     printf "\n"
 }
 
 cgr_exit_message() {
+    exit_code=$?
     printf "\n"
-    printf "################################################################################\n"
-    if [[ $? != 0 ]]; then
-        printf "# CGR SUBMIT: There was an error running the workflow\n"
+    cgr_bar
+    if [[ $exit_code == 0 ]]; then
+        printf "# CGR SUBMIT: Workflow complete %b%b%b\n" $UNICORN $PENGUIN $DNA
+    elif [[ $CLUSTER_KILLED == 1 ]]; then
+        printf "# CGR SUBMIT: The workflow was KILLED by the cluster. Check resource limits and re-run.\n"
+        printf "#             For example, try increasing the walltime using the `--time-hr` option\n"
+        printf "#             `cgr submit --time-hr 300 `\n"
+        printf "# Exit Code: %d\n" ${exit_code}
     else
-        printf "# CGR SUBMIT: Workflow complete\n"
+        printf "# CGR SUBMIT: There was a workflow error, check logs and re-run.\n"
+        printf "# Exit Code: %d\n" ${exit_code}
     fi
     printf "# End Time: %s\n# Date: %s\n" $(cgr_get_time)
-    printf "################################################################################\n"
+    cgr_bar
+    exit $exit_code
 }
 trap cgr_exit_message EXIT # capture exit signal and print exit message
+
+cgr_cluster_killed() {
+    exit_code=$?
+    CLUSTER_KILLED=1
+    exit $exit_code
+}
+{% if cgems %}
+trap cgr_cluster_killed USR1
+trap cgr_cluster_killed USR2
+{% endif %}
+{% if biowulf %}
+trap cgr_cluster_killed TERM
+{% endif %}
 
 # Make sure logs dir exists
 cd {{ working_dir }}
