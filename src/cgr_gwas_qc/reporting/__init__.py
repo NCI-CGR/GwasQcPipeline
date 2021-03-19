4a4016ddcdbb1b2833b498bd07a15292ac13a2d0
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-03-19 13:05:21 -0400

    feat: add useful mappers for reporting
    Adds a coupld of useful data types and mappers for reporting.

--- src/cgr_gwas_qc/reporting/__init__.py
+++ src/cgr_gwas_qc/reporting/__init__.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from jinja2 import Environment, PackageLoader
 
 env = Environment(
@@ -6,3 +7,15 @@ env = Environment(
     lstrip_blocks=True,
     keep_trailing_newline=True,
 )
+
+CASE_CONTROL_DTYPE = pd.CategoricalDtype(categories=["Case", "Control", "QC"])
+SEX_DTYPE = pd.CategoricalDtype(categories=["M", "F", "U"])
+
+# Mapping current column names to names from the legacy workflow to maintain
+# consistency in deliverables.
+REPORT_NAME_MAPPER = {
+    "expected_sex": "Expected_Sex",
+    "predicted_sex": "Predicted_Sex",
+    "sex_discordant": "Sex Discordant",
+    "X_inbreeding_coefficient": "ChrX_Inbreed_estimate",
+}
