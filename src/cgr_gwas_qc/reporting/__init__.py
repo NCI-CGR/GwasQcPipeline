b02f1469da6b3f5a2c3eb53fe12c38ea8093b6c4
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-03-29 09:36:16 -0400

    refactor: updated template location for reporting

--- src/cgr_gwas_qc/reporting/__init__.py
+++ src/cgr_gwas_qc/reporting/__init__.py
@@ -2,7 +2,7 @@ import pandas as pd
 from jinja2 import Environment, PackageLoader
 
 env = Environment(
-    loader=PackageLoader("cgr_gwas_qc", "reporting"),
+    loader=PackageLoader("cgr_gwas_qc", "reporting.templates"),
     trim_blocks=True,
     lstrip_blocks=True,
     keep_trailing_newline=True,
