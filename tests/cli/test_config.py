c1e2d1185dfacc28a7868374d0d5f71cb9e5b0af
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-04-01 07:59:16 -0400

    fix(config): fix output_pattern parser
    cgr-config had a bug producing the wrong `user_files.output_pattern`. I was working with file name stems but the parser was expecting the extension. I updated the folloing:
    
    - changed variable names to clearly show we are using file name stems
    - changed logic to use the stem instead of the file name

--- tests/cli/test_config.py
+++ tests/cli/test_config.py
@@ -8,11 +8,15 @@ def test_get_number_snps(bpm_file):
 
 
 @pytest.mark.parametrize(
-    "sample_sheet_name,output_pattern",
+    "sample_sheet_stem,output_pattern",
     [
-        ("test_AnalysisManifest_0123.csv", "{prefix}/test_{file_type}_0123.{ext}"),
-        ("test_0123.csv", "{prefix}/{file_type}.{ext}"),
+        (
+            "SR0001-001_1_AnalysisManifest_0000000000000",
+            "{prefix}/SR0001-001_1_{file_type}_0000000000000.{ext}",
+        ),
+        ("test_AnalysisManifest_0123", "{prefix}/test_{file_type}_0123.{ext}"),
+        ("test_0123", "{prefix}/{file_type}.{ext}"),
     ],
 )
-def test_get_output_pattern(sample_sheet_name, output_pattern):
-    assert output_pattern == get_output_pattern(sample_sheet_name)
+def test_get_output_pattern(sample_sheet_stem, output_pattern):
+    assert output_pattern == get_output_pattern(sample_sheet_stem)
