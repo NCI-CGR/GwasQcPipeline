1a20d1fec34e86e622810f61cbd25d2c1199d97a
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-05-19 14:14:32 -0400

    feat(testing): add new sample qc table comparison function
    Creates a function for comparing the legacy vs development versions of the sample qc table.

--- src/cgr_gwas_qc/testing/comparison.py
+++ src/cgr_gwas_qc/testing/comparison.py
@@ -2,6 +2,9 @@ from hashlib import sha256
 from math import isclose
 from pathlib import Path
 
+import pandas as pd
+from pandas.testing import assert_series_equal
+
 from cgr_gwas_qc.typing import PathLike
 
 
@@ -78,3 +81,110 @@ def assert_plink_fam_equal(file1: PathLike, file2: PathLike):
         AssertionError: if files are not identical.
     """
     assert file_hashes_equal(file1, file2)
+
+
+################################################################################
+# Major Workflow Files
+################################################################################
+def assert_legacy_dev_sample_qc_equal(legacy_file: PathLike, dev_file: PathLike):
+    legacy = pd.read_csv(legacy_file).set_index("Sample_ID").sort_index()
+    dev = pd.read_csv(dev_file).set_index("Sample_ID").sort_index()
+
+    assert legacy.shape[0] == dev.shape[0]
+
+    # --------------------
+    # IDATS
+    # --------------------
+    assert_series_equal(
+        legacy["IdatsInProjectDir"].replace({"YES": True, "NO": False}),
+        ~dev["is_missing_idats"],
+        check_dtype=False,
+        check_names=False,
+    )
+    assert_series_equal(legacy["IdatIntensity"], dev["IdatIntensity"])
+
+    # --------------------
+    # Call Rate
+    # --------------------
+    assert_series_equal(legacy["Call_Rate_Initial"], dev["Call_Rate_Initial"])
+    assert_series_equal(
+        legacy["Call_Rate_1_filter"].replace({"Y": True, "N": False}),
+        dev["is_cr1_filtered"],
+        check_dtype=False,
+        check_names=False,
+    )
+    assert_series_equal(legacy["Call_Rate_1"], dev["Call_Rate_1"])
+    assert_series_equal(
+        legacy["Call_Rate_2_filter"].replace({"Y": True, "N": False}),
+        dev["is_cr2_filtered"].fillna(True),  # dev sets samples as NA if filtered by cr1
+        check_dtype=False,
+        check_names=False,
+    )
+    assert_series_equal(legacy["Call_Rate_2"], dev["Call_Rate_2"])
+    assert_series_equal(
+        legacy["Low Call Rate"],
+        dev["is_call_rate_filtered"],
+        check_dtype=False,
+        check_names=False,
+    )
+
+    # --------------------
+    # Contamination
+    # --------------------
+    mask = dev["Contamination_Rate"].notna()
+    assert_series_equal(
+        legacy["Contamination_Rate"][mask], dev["Contamination_Rate"][mask], atol=1e-3
+    )
+    assert_series_equal(
+        legacy["Contaminated"], dev["is_contaminated"], check_dtype=False, check_names=False
+    )
+
+    # --------------------
+    # Sex
+    # --------------------
+    assert_series_equal(legacy["Expected_Sex"], dev["expected_sex"], check_names=False)
+    assert_series_equal(
+        legacy["Predicted_Sex"], dev["predicted_sex"].fillna("U"), check_names=False
+    )
+    assert_series_equal(
+        legacy["ChrX_Inbreed_estimate"], dev["X_inbreeding_coefficient"], check_names=False
+    )
+    assert_series_equal(
+        legacy["Sex Discordant"], dev["is_sex_discordant"], check_dtype=False, check_names=False
+    )
+
+    # --------------------
+    # Ancestry
+    # --------------------
+    assert_series_equal(legacy["AFR"], dev["AFR"], atol=0.1)
+    assert_series_equal(legacy["EUR"], dev["EUR"], atol=0.1)
+    assert_series_equal(legacy["ASN"], dev["ASN"], atol=0.1)
+
+    # --------------------
+    # Concordance
+    # --------------------
+    assert_series_equal(
+        legacy["Expected Replicate Discordance"],
+        dev["is_discordant_replicate"].fillna(False),
+        check_dtype=False,
+        check_names=False,
+    )
+
+    # --------------------
+    # Summary
+    # --------------------
+    assert_series_equal(legacy["Identifiler_Needed"], dev["identifiler_needed"], check_names=False)
+
+    # -----------------------
+    # Problematic Comparisons
+    # -----------------------
+
+    #  I am not including sex discordance in this count, maybe I should
+    # assert_series_equal(legacy["Count_of_QC_Issue"], dev["num_analytic_exclusion"], check_names=False)
+
+    # Ancestries are difference b/c very different methods
+    # assert_series_equal(legacy["Ancestry"], dev["Ancestry"])
+
+    # This is different and not sure why. Probably b/c I am counting based on
+    # PI_Subject_ID here and not SR_Subject_ID
+    # assert_series_equal(legacy["Count_of_SR_SubjectID"], dev["num_samples_per_subject"])
