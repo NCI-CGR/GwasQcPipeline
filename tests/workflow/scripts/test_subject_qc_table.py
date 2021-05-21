f3a19a1b1c3ef54cae3408ea8dc3ab5341e34997
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-05-21 14:56:00 -0400

    fix(workflow): remove additional sample exlcusion criteria
    I had added additional/configurable sample exlcusion criteria (Sex Discordant and Unexpected Replicate). The group said that these were not needed and we should stick to what was done in the legacy workflow.

--- tests/workflow/scripts/test_subject_qc_table.py
+++ tests/workflow/scripts/test_subject_qc_table.py
@@ -6,25 +6,8 @@ from cgr_gwas_qc.workflow.scripts import subject_qc_table
 @pytest.mark.real_data
 def test_subject_qc_table(real_data_cache, sample_qc_df):
     sample_concordance_csv = real_data_cache / "dev_outputs/sample_level/concordance/summary.csv"
-    df = (
-        subject_qc_table._sample_qc_to_subject_qc(sample_qc_df)
-        .pipe(subject_qc_table._add_unexpected_replicate_ids, sample_concordance_csv)
-        .pipe(subject_qc_table._add_analytic_exclusion, True, True)
+    df = subject_qc_table._sample_qc_to_subject_qc(sample_qc_df).pipe(
+        subject_qc_table._add_unexpected_replicate_ids, sample_concordance_csv
     )
     assert 0 == df.is_unexpected_replicate.sum()
     assert 3 == df.is_sex_discordant.sum()
-    assert 3 == df.subject_analytic_exclusion.sum()
-    assert "Sex Discordance" == df.subject_analytic_exclusion_reason.dropna().unique()[0]
-
-
-@pytest.mark.real_data
-def test_subject_qc_table_no_exclusions(real_data_cache, sample_qc_df):
-    sample_concordance_csv = real_data_cache / "dev_outputs/sample_level/concordance/summary.csv"
-    df = (
-        subject_qc_table._sample_qc_to_subject_qc(sample_qc_df)
-        .pipe(subject_qc_table._add_unexpected_replicate_ids, sample_concordance_csv)
-        .pipe(subject_qc_table._add_analytic_exclusion, False, False)
-    )
-    assert 0 == df.is_unexpected_replicate.sum()
-    assert 3 == df.is_sex_discordant.sum()
-    assert 0 == df.subject_analytic_exclusion.sum()
