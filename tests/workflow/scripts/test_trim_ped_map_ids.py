26512561eaf457e16f5149ca0c47e7dd09700b14
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-06-02 11:12:58 -0400

    test(workflow): fix use of raw strings in test

--- tests/workflow/scripts/test_trim_ped_map_ids.py
+++ tests/workflow/scripts/test_trim_ped_map_ids.py
@@ -14,5 +14,5 @@ def test_trim_map_ids(fake_data_cache):
     filename = fake_data_cache / "plink/samples.map"
     trimmed = trim_ped_map_ids.trim_map_ids(filename, 2)
     row = next(trimmed)
-    columns = row.split(r"\t")
+    columns = row.split("\t")
     assert 2 == len(columns[1])
