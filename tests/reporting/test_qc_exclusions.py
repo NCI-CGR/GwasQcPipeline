4ec25d9231785b692597ac80ca40657ec5d8a5b3
Justin Fear <1240432+jfear@users.noreply.github.com>
2021-06-16 12:10:50 -0400

    test(reporting): fix small formatting difference in test

--- tests/reporting/test_qc_exclusions.py
+++ tests/reporting/test_qc_exclusions.py
@@ -12,17 +12,17 @@ def exclusion_tables(sample_qc_df, subject_qc_df, agg_population_qc_df) -> Exclu
 @pytest.mark.real_data
 def test_sample_exclusion_table(exclusion_tables):
     expected = (
-        "| Filter Reason/Description      |   Controls |   Cases |   QC |   Other |   All Samples |\n"
-        "|:-------------------------------|-----------:|--------:|-----:|--------:|--------------:|\n"
-        "| User Excluded                  |          0 |       0 |    0 |       0 |             0 |\n"
-        "| Array Processing Failure       |          0 |       0 |    0 |       0 |             0 |\n"
-        "| Completion Rate 1st Filter     |          2 |       2 |    0 |       0 |             4 |\n"
-        "| Completion Rate 2nd Filter     |          7 |       3 |    0 |       0 |            10 |\n"
-        "| Contaminated                   |          0 |       0 |    0 |       0 |             0 |\n"
-        "| Internal QC Samples Removed    |          0 |       0 |    7 |       0 |             7 |\n"
-        "| Samples Remaining for Analysis |        137 |      62 |    0 |       0 |           199 |\n"
-        "| Expected Duplicates Removed    |          8 |       7 |    0 |       0 |            15 |\n"
-        "| Subjects Remaining             |        129 |      55 |    0 |       0 |           184 |"
+        "| Filter Reason/Description      |   Control |   Case |   QC |   Other |   All Samples |\n"
+        "|:-------------------------------|----------:|-------:|-----:|--------:|--------------:|\n"
+        "| User Excluded                  |         0 |      0 |    0 |       0 |             0 |\n"
+        "| Array Processing Failure       |         0 |      0 |    0 |       0 |             0 |\n"
+        "| Completion Rate 1st Filter     |         2 |      2 |    0 |       0 |             4 |\n"
+        "| Completion Rate 2nd Filter     |         7 |      3 |    0 |       0 |            10 |\n"
+        "| Contaminated                   |         0 |      0 |    0 |       0 |             0 |\n"
+        "| Internal QC Samples Removed    |         0 |      0 |    7 |       0 |             7 |\n"
+        "| Samples Remaining for Analysis |       137 |     62 |    0 |       0 |           199 |\n"
+        "| Expected Duplicates Removed    |         8 |      7 |    0 |       0 |            15 |\n"
+        "| Subjects Remaining             |       129 |     55 |    0 |       0 |           184 |"
     )
 
     assert expected == exclusion_tables.sample_exclusions
@@ -31,11 +31,11 @@ def test_sample_exclusion_table(exclusion_tables):
 @pytest.mark.real_data
 def test_subject_exclusion_table(exclusion_tables):
     expected = (
-        "| Filter Reason/Description   |   Controls |   Cases |   QC |   Other |   All Subjects |\n"
-        "|:----------------------------|-----------:|--------:|-----:|--------:|---------------:|\n"
-        "| Sex Discordant              |          3 |       0 |    0 |       0 |              3 |\n"
-        "| Unexpected Replicates       |          0 |       0 |    0 |       0 |              0 |\n"
-        "| Autosomal Het               |          0 |       0 |    0 |       0 |              0 |"
+        "| Filter Reason/Description   |   Control |   Case |   QC |   Other |   All Subjects |\n"
+        "|:----------------------------|----------:|-------:|-----:|--------:|---------------:|\n"
+        "| Sex Discordant              |         3 |      0 |    0 |       0 |              3 |\n"
+        "| Unexpected Replicates       |         0 |      0 |    0 |       0 |              0 |\n"
+        "| Autosomal Het               |         0 |      0 |    0 |       0 |              0 |"
     )
 
     assert expected == exclusion_tables.subject_exclusions
