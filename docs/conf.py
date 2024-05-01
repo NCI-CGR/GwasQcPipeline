5d1e54650bbcb632b653048b06eada129650bef4
jaamarks <jmarks@rti.org>
2024-05-01 09:18:33 -0400

    docs: update conf.py with author information
    Add RTI International contributors to the author list.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ from cgr_gwas_qc.version import __version__
 
 project = "GwasQcPipeline"
 copyright = "2020, Leidos Biomedical Research, Inc."
-authors = ["Justin Fear", "Eric Karlins", "Jiahui Wang", "Cameron Palmer", "Bari Ballew", "Bin Zhu"]
+authors = ["Justin Fear", "Eric Karlins", "Jiahui Wang", "Cameron Palmer", "Bari Ballew", "Bin Zhu", "Caryn Willis", "Kevin Liao", "Jesse Marks"]
 
 # The full version, including alpha/beta/rc tags
 release = (
