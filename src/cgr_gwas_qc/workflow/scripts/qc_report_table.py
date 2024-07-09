2d85c860a4c933a4d62cd7bab793b9f1057c68a3
jaamarks <jmarks@rti.org>
2024-07-09 14:43:12 -0400

    Implement logic for calculating total QC issues
    - Introduces functionality to calculate the total number of QC issues
      present in a sample.
    - Addresses missing data in the "Count_of_QC_Issue" column of
      QC_Report.xlsx.
    - Calculates the number of "TRUE" occurrences in five boolean columns:
        "Low Call Rate",
        "Contaminated",
        "Expected Replicate Discordance",
        "Unexpected Replicate",
        and "Sex Discordant".

--- src/cgr_gwas_qc/workflow/scripts/qc_report_table.py
+++ src/cgr_gwas_qc/workflow/scripts/qc_report_table.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 import typer
@@ -28,7 +29,6 @@ def main(
     graf: Path,
     outfile: Path,
 ):
-
     with pd.ExcelWriter(outfile) as writer:
         _sample_qc(sample_sheet_csv, sample_qc_csv).to_excel(
             writer, sheet_name="SAMPLE_QC", index=False
@@ -83,14 +83,30 @@ _SAMPLE_QC_COLUMNS = [
 ]
 
 
+_PASS_FAIL_QC_COLUMNS: List[str] = [
+    "is_call_rate_filtered",
+    "is_contaminated",
+    "Expected Replicate Discordance",
+    "Unexpected Replicate",
+    "is_sex_discordant",
+]
+
+
+def _count_issues(sample_qc_csv: PathLike, qc_columns: List[str]) -> pd.DataFrame:
+    df = sample_qc_table.read(sample_qc_csv)
+    df["Count_of_QC_Issue"] = df[qc_columns].sum(axis=1)
+    return df
+
+
 def _sample_qc(sample_sheet_csv: PathLike, sample_qc_csv: PathLike) -> pd.DataFrame:
     ss = sample_sheet.read(sample_sheet_csv, remove_exclusions=False).rename(
         REPORT_NAME_MAPPER, axis=1
     )
     _additional_columns = [x for x in ss.columns if x not in _SAMPLE_QC_COLUMNS]
+
+    df = _count_issues(sample_qc_csv, _PASS_FAIL_QC_COLUMNS)
     return (
-        sample_qc_table.read(sample_qc_csv)
-        .rename(REPORT_NAME_MAPPER, axis=1)
+        df.rename(REPORT_NAME_MAPPER, axis=1)
         .merge(ss, on="Sample_ID", suffixes=["", "_DROP"], how="outer")
         .filter(regex="^(?!.*_DROP)")
         .reindex(_SAMPLE_QC_COLUMNS + _additional_columns, axis=1)
