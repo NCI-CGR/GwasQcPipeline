73b0215fe85a6b42eec11b6af95f4c1ec0dc5d04
Kevin Liao <liaoks@ccad2.cm.cluster>
2024-08-06 13:19:40 -0400

    update testing sample conconrdance

--- tests/workflow/scripts/test_sample_concordance.py
+++ tests/workflow/scripts/test_sample_concordance.py
@@ -10,15 +10,11 @@ def concordance():
     data_cache = FakeData()
     return sample_concordance.build(
         data_cache / "cgr/concordance.csv",
-        data_cache / "graf/relatedness.tsv",
-        data_cache / "king/king.kin0",
     )
 
 
 def test_build(concordance):
     assert 4 == concordance.filter(regex="^PLINK").shape[1]
-    assert 3 == concordance.filter(regex="^GRAF").shape[1]
-    assert 2 == concordance.filter(regex="^KING").shape[1]
     assert ["Sample_ID1", "Sample_ID2"] == concordance.index.names
 
 
@@ -91,36 +87,20 @@ def test_add_discordant_replicate_missing_pair(fake_cfg, concordance):
 
 
 @pytest.mark.parametrize(
-    "expected_result,is_rep,plink,graf,king",
+    "expected_result,is_rep,plink",
     [
-        (False, False, True, "ID", "ID"),  # Not an expected replicate
+        (False, False, True),  # Not an expected replicate
         # PLINK Concordant
-        (False, True, True, "ID", "ID"),  # all concordant
-        (False, True, True, "UN", "UN"),  # GRAF and KING don't matter
-        (False, True, True, pd.NA, pd.NA),  # GRAF and KING don't matter
+        (False, True, True),  # all concordant
         # PLINK Discordant
-        (True, True, False, "ID", "UN"),  # GRAF and KING don't matter
-        (True, True, False, "UN", "ID"),  # GRAF and KING don't matter
-        (True, True, False, pd.NA, pd.NA),  # GRAF and KING don't matter
-        (True, True, False, "UN", "UN"),  # GRAF and KING don't matter
-        # PLINK Missing
-        (False, True, pd.NA, "ID", "ID"),  # GRAF and KING concordant
-        (False, True, pd.NA, "ID", "UN"),  # GRAF and KING disagree
-        (False, True, pd.NA, "UN", "ID"),  # GRAF and KING disagree
-        (False, True, pd.NA, "ID", pd.NA),  # GRAF concordant
-        (False, True, pd.NA, pd.NA, "ID"),  # KING concordant
-        (True, True, pd.NA, "UN", pd.NA),  # GRAF discordant
-        (True, True, pd.NA, pd.NA, "UN"),  # KING discordant
-        (True, True, pd.NA, pd.NA, pd.NA),  # All missing: flag as discordant replicate
+        (True, True, False),  # GRAF and KING don't matter
     ],
 )
-def test_add_discordant_logic(expected_result, is_rep, plink, graf, king):
+def test_add_discordant_logic(expected_result, is_rep, plink):
     df = pd.DataFrame(
         {
             "is_expected_replicate": [is_rep],
             "PLINK_is_ge_concordance": [plink],
-            "GRAF_relationship": [graf],
-            "KING_relationship": [king],
         }
     )
 
@@ -149,18 +129,13 @@ def test_add_unexpected_replicate_one(fake_cfg, concordance):
 
 
 @pytest.mark.parametrize(
-    "expected_result,is_rep,plink,graf,king",
+    "expected_result,is_rep,plink",
     [
-        (False, True, True, "ID", "ID"),  # expected replicate
-        (True, False, True, "ID", "ID"),  # All concordant
-        (True, False, True, "UN", "UN"),  # plink concordant
-        (True, False, False, "ID", "UN"),  # graf concordant
-        (True, False, False, "UN", "ID"),  # king concordant
-        (True, False, True, pd.NA, pd.NA),  # plink concordant with missing
-        (True, False, pd.NA, "ID", pd.NA),  # graf concordant with missing
-        (True, False, pd.NA, pd.NA, "ID"),  # king concordant with missing
-        (False, False, False, "UN", "UN"),  # all discordant
-        (False, False, pd.NA, pd.NA, pd.NA),  # all missing
+        (False, True, True),  # expected replicate
+        (True, False, True),  # All concordant
+        (True, False, True),  # plink concordant
+        (False, False, False),  # all discordant
+        (False, False, pd.NA),  # all missing
     ],
 )
 def test_add_unexpected_logic(expected_result, is_rep, plink, graf, king):
@@ -168,8 +143,6 @@ def test_add_unexpected_logic(expected_result, is_rep, plink, graf, king):
         {
             "is_expected_replicate": [is_rep],
             "PLINK_is_ge_concordance": [plink],
-            "GRAF_relationship": [graf],
-            "KING_relationship": [king],
         }
     )
 
