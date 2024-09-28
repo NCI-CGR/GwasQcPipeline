2de27eb98eb1b3e5af9fa479f3217b25094bb09d
jaamarks <jmarks@rti.org>
2024-09-27 22:48:16 -0400

    test: Add `skipif` decorator to handle CGEMS directory access
    This update introduces the `skipif` decorator in the test to ensure that
    if the user does not have access to the CGEMS_QC_DIR, the test will be
    skipped instead of failing.

--- tests/cli/test_config.py
+++ tests/cli/test_config.py
@@ -1,12 +1,27 @@
+import os
+
+import pytest
+
 from cgr_gwas_qc.cli.config import CGEMS_QC_DIR, _get_cgems_production_run_dir
 
+# Define the condition for skipping the test
+skip_condition = not os.path.exists(CGEMS_QC_DIR) or not os.access(CGEMS_QC_DIR, os.R_OK)
 
+
+@pytest.mark.skipif(
+    skip_condition,
+    reason=f"Skipping test because {CGEMS_QC_DIR} does not exist or is not accessible.",
+)
 def test_get_cgems_production_run_dir(monkeypatch):
     monkeypatch.setattr("cgr_gwas_qc.cli.config.TODAY", "01012021")
     test_path = _get_cgems_production_run_dir("TestProject")
     assert CGEMS_QC_DIR / "TestProject/builds/QC_v1_01012021" == test_path
 
 
+@pytest.mark.skipif(
+    skip_condition,
+    reason=f"Skipping test because {CGEMS_QC_DIR} does not exist or is not accessible.",
+)
 def test_get_cgems_production_run_dir_increment_if_previous_runs(monkeypatch):
     def glob(*args, **kwargs):
         """Return 2 previous runs"""
